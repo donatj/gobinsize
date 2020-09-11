dd4ba99071449701849c84bd4feae894013357d1
wy <xiaobagou@gmail.com>
2020-09-11 16:45:08 +0800

    Update buildall.sh
    Add go version 1.13-1.15

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for VER in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12
+for VER in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15
 do
 	echo "Building with golang:$VER"
 	docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:"$VER" go build -o output/hello."$VER" ./hello
