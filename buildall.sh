0ca6303c08c1a18733525f1b4bd25750b28975f9
Jesse Donat <donatj@gmail.com>
2023-02-27 12:56:27 -0600

    Fix output naming

--- buildall.sh
+++ buildall.sh
@@ -8,7 +8,7 @@ mkdir -p output
 rm -f output/*
 
 build() {
-	docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp/"$2" golang:"$1" go build -o ../output/hello."$VER" .
+	docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp/"$2" golang:"$1" go build -o ../output/"$2"."$VER" .
 }
 
 for VER in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20
