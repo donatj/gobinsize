2735ee22bcf757fc25dfce4e3f34e6d910090d9e
Jesse Donat <donatj@gmail.com>
2023-02-27 12:24:06 -0600

    Append additional versions

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for VER in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15
+set -e -x
+
+for VER in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20
 do
 	echo "Building with golang:$VER"
 	docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:"$VER" go build -o output/hello."$VER" ./hello
