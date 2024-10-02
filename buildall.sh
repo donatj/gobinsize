43de0f95bad7bdcfdc3bfc3e2004cc4b87455d7a
Jesse Donat <donatj@gmail.com>
2024-10-02 17:45:18 -0500

    Add Go 1.21, 1.22, 1.23

--- buildall.sh
+++ buildall.sh
@@ -12,7 +12,7 @@ build() {
 	docker run --rm -e GOPATH=/usr/src/myapp/gopath -v "$PWD":/usr/src/myapp -w /usr/src/myapp/gopath/src/"$2" golang:"$1" /bin/bash -c "go get && go build -o /usr/src/myapp/output/$3.$1"
 }
 
-for VER in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20
+for VER in 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20 1.21 1.22 1.23
 do
 	echo "Building with golang:$VER"
 
